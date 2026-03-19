f5b268fea1763f101ea87222ed6d947703774459
Anil Kumar <130557164+AnilKumarchotu@users.noreply.github.com>
2026-03-19 10:37:01 +0530

    Other routes also changed.

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,33 +1,32 @@
 import { connectDB } from "@/lib/mongodb";
 import Ticket from "@/models/Ticket";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
+// ✅ GET ALL TICKETS
 export async function GET() {
   await connectDB();
+
   const tickets = await Ticket.find();
+
   return NextResponse.json(tickets);
 }
 
-export async function POST(req: Request) {
-
+// ✅ CREATE TICKET
+export async function POST(req: NextRequest) {
   try {
+    await connectDB(); // 🔥 IMPORTANT
 
+    const body = await req.json();
 
-    const body = await req.json()
+    const ticket = await Ticket.create(body);
 
-    const ticket = await Ticket.create(body)
-
-    return NextResponse.json(ticket, { status: 201 })
-
+    return NextResponse.json(ticket, { status: 201 });
   } catch (error) {
-
-    console.error("POST ERROR:", error)
+    console.error("POST ERROR:", error);
 
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
-    )
-
+    );
   }
-
 }
